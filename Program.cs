ff8cdf0eb16716f1d1bec7302b4ef4aaa792dfe2
Denis Chornonog <onlinesvxonline@mail.ru>
2024-04-09 16:02:08 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program 
+    { 
+        static void Main(string[] args)
+        { 
+            int[,] arr = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
+            
+            int a = arr.GetLength(0);
+            int b = arr.GetLength(1);
+
+            int[] sort = new int[a * b];
+
+            int c = 0;
+
+            for (int i = 0; i < a; i++) 
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    sort[c] = arr[i, j];
+                    c++;
+                }
+            }
+            Array.Sort(sort);
+            c = 0;
+            for (int i = 0;i < a; i++)
+            {
+                for (int j = 0;j < b; j++)
+                {
+                    arr[i, j] = sort[c];
+                    c++;
+
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        } 
+    }
+}
